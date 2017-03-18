8757681147fc92e46f8c89b472845e82a27b5cdd
DovileP <dovilepreidyte@gmail.com>
2017-03-18 19:27:57 +0200

    Add files via upload

---
+++ InputDevice.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.io.IOException;
+
+public class InputDevice
+{
+
+    public String doInput( )
+	{
+		InputStreamReader isr = new InputStreamReader ( System.in );
+		BufferedReader br = new BufferedReader ( isr );
+		String inputText = "";
+		try
+		{
+			inputText = br.readLine();
+		}
+		catch (IOException e)
+		{
+		    System.exit( 0 );
+		}
+		return inputText;
+	};
+
+}
